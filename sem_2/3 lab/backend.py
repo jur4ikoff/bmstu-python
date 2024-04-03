8cbf3cb94d595314adb370ae8c0f67ec93e0541b
1234 <1234>
2024-04-03 21:28:08 +0300

    debug

---
+++ sem_2/3 lab/backend.py
@@ -0,0 +1,76 @@
+import PIL.Image as Image
+import numpy
+import os
+import numpy as np
+
+
+# encrypting and decrypting
+
+
+def encrypt(text, file):
+    img = Image.open(file)
+    widht, height = img.size[0], img.size[1]
+    arr = numpy.array(img)
+    bit_texts = [bin(i)[2:].zfill(8) for i in text.encode() + b'\x00']
+
+    flag = False
+    cur_byte, cur_bit = 0, 7
+    for i in range(widht):
+        if flag:
+            break
+        for j in range(height):
+            if flag:
+                break
+            for k in range(3):
+                if cur_bit < 0:
+                    cur_byte += 1
+                    cur_bit = 7
+                if cur_byte >= len(bit_texts):
+                    flag = True
+                    break
+
+                bit = bit_texts[cur_byte][cur_bit]
+                cur_bit -= 1
+                print(1)
+                if bit == '0':
+                    arr[i, j, k] &= 254
+                else:
+                    arr[i, j, k] |= 1
+
+    new_image = Image.fromarray(arr)
+    new_image.save("encode_image.bmp")
+    return os.path.abspath("encode_image.bmp")
+
+
+def decrypt(file):
+    img = Image.open(file)
+    widht, height = img.size[0], img.size[1]
+    new_bytes = b""
+    arr = numpy.array(img)
+    byte = 0
+    cur_bit = 0
+    flag = False
+    for i in range(widht):
+        if flag:
+            break
+        for j in range(height):
+            if flag:
+                break
+            for k in range(3):
+                if cur_bit == 8:
+                    if byte == 0:
+                        flag = True
+                        break
+                    cur_bit = 0
+                    new_bytes += int(byte).to_bytes(1, "little")
+                    byte = 0
+
+                byte <<= 1
+                byte += arr[i, j, k] & 1
+                cur_bit += 1
+
+    print(new_bytes.decode())
+
+
+encrypt("хуйня", "Image.bmp")
+# print(decrypt(os.path.abspath("encode_image.bmp")))
--- sem_2/3 lab/frontend.py
+++ sem_2/3 lab/frontend.py
@@ -1,14 +1,15 @@
 import warnings
-# from backend import calculate
 from sys import argv, exit
 from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QMessageBox, QDialog, QVBoxLayout
+from fnmatch import fnmatch
+from backend import encrypt, decrypt
+from PyQt6.QtWidgets import QApplication, QMessageBox, QDialog, QVBoxLayout, QFileDialog
 from PyQt6.QtWidgets import QMainWindow, QTableWidgetItem, QTableWidget
 from PyQt6.QtGui import QPixmap
 
 warnings.filterwarnings('ignore')
 
-WEIGHT, HEIGHT = 800, 560
+WEIGHT, HEIGHT = 800, 740
 
 
 class MainWindow(QMainWindow):
@@ -18,37 +19,90 @@ class MainWindow(QMainWindow):
         super().__init__()
         uic.loadUi('interface.ui', self)
         self.initUI()
-        self.binding_keys()
-        self.load_picture()
 
     def initUI(self):
         """base settings"""
+        self.binding_keys()
+        self.status = 0
+        self.mode = 0
+        self.file = -2
         self.setFixedSize(WEIGHT, HEIGHT)
         self.setWindowTitle('Стеганография')
 
     def binding_keys(self):
         '''Binding Buttons'''
         self.AuthorMenu.triggered.connect(self.show_author_info)
-        # self.ConvertButton.clicked.connect(self.convert)
-
-    def button_input(self):
-        """Считывание клавиш"""
-        text = self.sender().text()
-        text_on_monitor = self.Monitor.text()
-        self.Monitor.setText(text_on_monitor + text)
+        self.comboBox.activated.connect(self.on_combobox_activated)
+        self.chooseFile.clicked.connect(self.open_file_dialog)
+        self.convertButton.clicked.connect(self.convert)
 
     def show_author_info(self):
         '''Вывод информации об авторе'''
         author_info = "Выполнил Попов Ю.А. ИУ7-22Б"
         QMessageBox.information(self, 'Об авторе', author_info)
 
-    def load_picture(self):
-        pixmap = QPixmap('image.bmp')  # Укажите путь к изображению
-        scaled_pixmap = pixmap.scaled(640, 360)
-        label = self.showImage
-        label.setPixmap(pixmap)
-        label = self.showImage2
-        label.setPixmap(pixmap)
+    def on_combobox_activated(self):
+        selected_text = self.comboBox.currentText()
+        if selected_text == "Шифрование":
+            self.mode = 0
+        else:
+            self.mode = 1
+        self.set_status_label(selected_text, 0)
+
+    def load_picture(self, image):
+        if fnmatch(image, "*.bmp"):
+            pixmap = QPixmap(image)
+            label = self.showImage
+            label.setPixmap(pixmap)
+            label.setScaledContents(True)
+            self.status += 1
+        else:
+            self.file = -1
+
+    def open_file_dialog(self):
+        file_dialog = QFileDialog(self)
+        file_dialog.setNameFilter("Text files (*.bmp);;All files (*.*)")
+
+        if file_dialog.exec():
+            selected_file = file_dialog.selectedFiles()
+            self.file = selected_file[0]
+            self.set_status_label(f"Выбран файл: {self.file}", 1)
+            self.load_picture(self.file)
+        else:
+            self.file = -1
+
+    def set_status_label(self, text, label):
+        if label == 0:
+            self.statusLabel.setText(text)
+        elif label == 1:
+            self.fileStatus.setText(text)
+
+    def get_lineEdit_text(self):
+        self.entered_text = self.textToCrypt.toPlainText()
+        self.status += 1
+
+    def convert(self):
+        self.get_lineEdit_text()
+        if self.status < 2:
+            print(self.file, self.entered_text, self.status)
+            self.status = 0
+
+            self.set_status_label(f"Ошибка в данных", 0)
+            return 1
+
+        print(self.mode,  self.file)
+        if self.mode == 0:
+            try:
+                self.path = encrypt(self.entered_text, self.file)
+                self.load_picture(self.path)
+                self.mode = 1
+                self.set_status_label(f"Зашифровано, режим дешифровки", 0)
+                self.set_status_label(f"Выбран файл: {self.path}", 1)
+            except Exception as e:
+                self.set_status_label(f"Произошла ошибка {e}", 0)
+        elif self.mode == 1:
+            decrypt(self.path)
+
 
 
 
